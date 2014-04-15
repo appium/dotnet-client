d753f3c7c0f000d46348b112cc2f2aa517880366
sebv <seb.vincent@gmail.com>
2014-04-15 18:09:08 +0800

    Added ByAndroidUIAutomator strategy

--- src/Appium/AppiumDriver.cs
+++ src/Appium/AppiumDriver.cs
@@ -148,6 +148,41 @@ namespace OpenQA.Selenium.Appium
 
 		#endregion
 
+		#region IFindsByAndroidUIAutomator Members
+		/// <summary>
+		/// Finds the first element in the page that matches the Android UIAutomator selector supplied
+		/// </summary>
+		/// <param name="selector">Selector for the element.</param>
+		/// <returns>IWebElement object so that you can interact that object</returns>
+		/// <example>
+		/// <code>
+		/// IWebDriver driver = new RemoteWebDriver(DesiredCapabilities.Firefox());
+		/// IWebElement elem = driver.FindElementByAndroidUIAutomator('elements()'))
+		/// </code>
+		/// </example>
+		public IWebElement FindElementByAndroidUIAutomator(string selector)
+		{
+			return this.FindElement("-android uiautomator", selector);
+		}
+
+		/// <summary>
+		/// Finds a list of elements that match the Android UIAutomator selector supplied
+		/// </summary>
+		/// <param name="selector">Selector for the elements.</param>
+		/// <returns>ReadOnlyCollection of IWebElement object so that you can interact with those objects</returns>
+		/// <example>
+		/// <code>
+		/// IWebDriver driver = new RemoteWebDriver(DesiredCapabilities.Firefox());
+		/// ReadOnlyCollection<![CDATA[<IWebElement>]]> elem = driver.FindElementsByAndroidUIAutomator(elements())
+		/// </code>
+		/// </example>
+		public ReadOnlyCollection<IWebElement> FindElementsByAndroidUIAutomator(string selector)
+		{
+			return this.FindElements("-android uiautomator", selector);
+		}
+
+		#endregion
+
         /// <summary>
         /// Shakes the device.
         /// </summary>
--- test/SelectorTest.cs
+++ test/SelectorTest.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace OpenQA.Selenium.Appium
 {
 	[TestFixture ()]
-	public class SelectorTest
+	public class ByIosUIAutomationTest
 	{	
 		public FakeAppium server;
 		public AppiumDriver driver;
@@ -73,5 +73,75 @@ namespace OpenQA.Selenium.Appium
 		}
 
 	}
+
+	[TestFixture ()]
+	public class ByAndroidUIAutomatorTest
+	{	
+		public FakeAppium server;
+		public AppiumDriver driver;
+
+		[TestFixtureSetUp]
+		public void RunBeforeAll(){
+			server = new FakeAppium (4744);			 
+			server.Start ();
+			server.respondToInit ();
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			driver = new AppiumDriver (new Uri("http://127.0.0.1:4744/wd/hub"), capabilities);
+			server.clear ();
+		}
+
+		[TestFixtureTearDown]
+		public void RunAfterAll(){
+			server.Stop ();
+		}
+
+		[TearDown]
+		public void RunAfterEach()
+		{
+			server.clear ();
+		}
+
+		[Test]
+		public void FindElementByAndroidUIAutomator ()
+		{			 
+			server.respondTo ("POST", "/element", new Dictionary<string, object>  {
+				{"ELEMENT", '5'}
+			});
+			IWebElement element = driver.FindElementByAndroidUIAutomator (".elements()");
+			server.clear ();
+			server.respondTo ("GET", "/element/5/attribute/id", "1234");
+			element.GetAttribute ("id");
+		}
+
+		[Test]
+		public void FindElementsByAndroidUIAutomator ()
+		{		
+			List<object> results = new List<object>();
+			results.Add (new Dictionary<string, object> {{"ELEMENT", "4"}});
+			results.Add (new Dictionary<string, object> {{"ELEMENT", "6"}});
+			results.Add (new Dictionary<string, object> {{"ELEMENT", "8"}});
+			server.respondTo ("POST", "/elements", results);
+			ICollection<IWebElement> elements = driver.FindElementsByAndroidUIAutomator (".elements()");
+			Assert.AreEqual (elements.Count, 3); 
+		}
+
+		[Test]
+		public void ByAndroidUIAutomator ()
+		{			 
+			server.respondTo ("POST", "/element", new Dictionary<string, object>  {
+				{"ELEMENT", '5'}
+			});
+			driver.FindElement(new ByAndroidUIAutomator(".elements()"));
+			(new ByAndroidUIAutomator(".elements()")).FindElement(driver);
+			server.clear ();
+			List<object> results = new List<object>();
+			results.Add (new Dictionary<string, object> {{"ELEMENT", "4"}});
+			server.respondTo ("POST", "/elements", results);
+			driver.FindElements(new ByAndroidUIAutomator(".elements()"));
+			(new ByAndroidUIAutomator(".elements()")).FindElements(driver);
+		}
+
+	}
+
 }
 
