06115ef6d5d125b286237bb8c11ff046a52d657a
Astro03 <locopelon03@hotmail.com>
2014-04-18 19:32:00 -0700

    Added the webdriver and testapp projects for easier debugging. Added code to start multi touch but I'm still unsure how to get things to send since items such as RemoteTouchScreen.Down() do not work.

---
+++ src/Appium/MultiTouch/RemoteMultiTouchScreen.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
+
+namespace OpenQA.Selenium.Appium.MultiTouch
+{
+    public class RemoteMultiTouchScreen : RemoteTouchScreen
+    {
+        private readonly AppiumDriver _Driver;
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RemoteTouchScreen"/> class.
+        /// </summary>
+        /// <param name="driver">The <see cref="RemoteWebDriver"/> for which the touch screen will be managed.</param>
+        public RemoteMultiTouchScreen(AppiumDriver driver)
+            : base(driver)
+        {
+            _Driver = driver;
+        }
+        #endregion Constructor
+
+        #region Public Methods
+
+        #region Pinch
+        /// <summary>
+        /// Pinch at the specified region
+        /// </summary>
+        /// <param name="x">x screen location</param>
+        /// <param name="y">y screen location</param>
+        public void Pinch(int x, int y)
+        {
+            _Driver.Pinch(x, y);
+        }
+
+        #endregion Pinch
+
+        #region Zoom
+        /// <summary>
+        /// Zoom at the specified point
+        /// </summary>
+        /// <param name="x">x screen location</param>
+        /// <param name="y">y screen location</param>
+        public void Zoom(int x, int y)
+        {
+            _Driver.Zoom(x,y);
+        }
+        #endregion Zoom
+
+        #endregion Public Methods
+    }
+}
