d3987e4853bc6f1523224638f3f1ef170f052fa6
Astro03 <locopelon03@hotmail.com>
2014-05-21 10:37:54 -0700

    Fix FindByAccessibilityId bug where you were not to find a subelement of an element in a dom

---
+++ src/Appium/Interfaces/IFindByAccessibilityId.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace OpenQA.Selenium.Appium.Interfaces
+{
+    internal interface IFindByAccessibilityId
+    {
+        /// <summary>
+        /// Finds the first of elements that match the Accessibility Id selector supplied
+        /// </summary>
+        /// <param name="selector">an Accessibility Id selector</param>
+        /// <returns>IWebElement object so that you can interact that object</returns>
+        IWebElement FindElementByAccessibilityId(string selector);
+
+        /// <summary>
+        /// Finds a list of elements that match the Accessibility Id selector supplied
+        /// </summary>
+        /// <param name="selector">an Accessibility Id selector</param>
+        /// <returns>IWebElement object so that you can interact that object</returns>
+        ReadOnlyCollection<IWebElement> FindElementsByAccessibilityId(string selector);
+    }
+}
