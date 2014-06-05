a1b20935d10c41354f42a35e59b227aad458c7d9
sebv <seb.vincent@gmail.com>
2014-06-05 19:21:27 +0800

    added samples

---
+++ samples/Basics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium;
+
+namespace OpenQA.Selenium.Appium.Samples
+{
+	public class Basics
+	{
+		public Basics ()
+		{
+		}
+
+		public void Run (string appPath) {
+			// set up the remote web driver
+			Console.WriteLine("Connecting to Appium server");
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.SetCapability("appium-version", "1.0");
+			capabilities.SetCapability("platformName", "iOS");
+			capabilities.SetCapability("platformVersion", "7.1");
+			capabilities.SetCapability("deviceName", "iPhone Simulator");
+			capabilities.SetCapability("app", appPath);
+			AppiumDriver driver = new AppiumDriver(new Uri("http://127.0.0.1:4723/wd/hub"), capabilities);		
+			driver.Quit();
+		}
+
+
+	}
+}
+
---
+++ samples/Runner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace OpenQA.Selenium.Appium.Samples
+{
+	class MainClass
+	{
+		private static string getProjectRootPath()
+		{
+			return Regex.Replace(System.Reflection.Assembly.GetExecutingAssembly().Location, "/samples/.*$", "/samples");
+			//return appiumDir + "/sample-code/apps/TestApp/build/Release-iphonesimulator/TestApp.app";
+		}
+
+
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			// Console.WriteLine (getProjectRootPath());
+			(new Basics()).Run(getProjectRootPath() + "/assets/TestApp.zip");
+		}
+	}
+}
