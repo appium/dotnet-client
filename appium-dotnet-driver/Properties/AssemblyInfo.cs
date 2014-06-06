74d4e13e04da186bb257b518824f8733a57e6c04
Astro David Ashtaralnakhai <Astro03@users.noreply.github.com>
2014-06-06 14:40:31 -0700

    Update AssemblyInfo.cs

--- appium-dotnet-driver/Properties/AssemblyInfo.cs
+++ appium-dotnet-driver/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("lib")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("appium-dotnet-driver")]
+[assembly: AssemblyDescription ("Appium Dotnet Driver")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("baba")]
+[assembly: AssemblyCompany ("Appium Contributors")]
+[assembly: AssemblyProduct ("Appium-Dotnet-Driver")]
+[assembly: AssemblyCopyright ("Copyright © 2014")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
