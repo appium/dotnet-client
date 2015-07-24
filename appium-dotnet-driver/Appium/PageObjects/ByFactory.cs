e4f73df656ca58f022183edcf35e2a36943f8287
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2015-07-24 14:52:14 +0300

    #64 Copy-Pasted code was replaced.
    Now reflection is being used here.

--- appium-dotnet-driver/Appium/PageObjects/ByFactory.cs
+++ appium-dotnet-driver/Appium/PageObjects/ByFactory.cs
@@ -19,48 +19,10 @@ namespace OpenQA.Selenium.Appium.PageObjects
     {
         private static By From(FindsByAttribute attribute)
         {
-            var how = attribute.How;
-            var usingValue = attribute.Using;
-            switch (how)
-            {
-                case How.Id:
-                    return By.Id(usingValue);
-                case How.Name:
-                    return By.Name(usingValue);
-                case How.TagName:
-                    return By.TagName(usingValue);
-                case How.ClassName:
-                    return By.ClassName(usingValue);
-                case How.CssSelector:
-                    return By.CssSelector(usingValue);
-                case How.LinkText:
-                    return By.LinkText(usingValue);
-                case How.PartialLinkText:
-                    return By.PartialLinkText(usingValue);
-                case How.XPath:
-                    return By.XPath(usingValue);
-                case How.Custom:
-                    if (attribute.CustomFinderType == null)
-                    {
-                        throw new ArgumentException("Cannot use How.Custom without supplying a custom finder type");
-                    }
-
-                    if (!attribute.CustomFinderType.IsSubclassOf(typeof(By)))
-                    {
-                        throw new ArgumentException("Custom finder type must be a descendent of the By class");
-                    }
-
-                    ConstructorInfo ctor = attribute.CustomFinderType.GetConstructor(new Type[] { typeof(string) });
-                    if (ctor == null)
-                    {
-                        throw new ArgumentException("Custom finder type must expose a public constructor with a string argument");
-                    }
-
-                    By finder = ctor.Invoke(new object[] { usingValue }) as By;
-                    return finder;
-            }
-
-            throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "Did not know how to construct How from how {0}, using {1}", how, usingValue));
+            Assembly assembly = Assembly.LoadFrom("WebDriver.Support.dll");
+            Type seleniumByFactory = assembly.GetType("OpenQA.Selenium.Support.PageObjects.ByFactory");
+            MethodInfo m = seleniumByFactory.GetMethod("From", new Type[] { typeof(FindsByAttribute) });
+            return (By) m.Invoke(seleniumByFactory, new object[] { attribute });
         }
 
         private static ReadOnlyCollection<By> CreateDefaultLocatorList(MemberInfo member)
