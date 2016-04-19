ad6fa7f26ddc44f3f3d14fc11b48ff842e894002
Jonathan Stoneman <jon@stoneman.me.uk>
2016-04-19 22:29:16 +0100

    Initial support for Windows devices running WinAppDriver
    https://github.com/Microsoft/WinAppDriver

---
+++ appium-dotnet-driver/Appium/Enums/WindowsKeyCodes.cs
@@ -0,0 +1,24 @@
+﻿namespace OpenQA.Selenium.Appium.Windows.Enums
+{
+    public sealed class WindowsKeyCode
+    {
+        public static readonly int Power = 0;
+        public static readonly int Windows = 1;
+        public static readonly int VolumeUp = 2;
+        public static readonly int VolumeDown = 3;
+        public static readonly int RotationLock = 4;
+        public static readonly int CountMin = 5;
+        public static readonly int Back = 5;
+        public static readonly int Search = 6;
+        public static readonly int CameraFocus = 7;
+        public static readonly int CameraShutter = 8;
+        public static readonly int RingerToggle = 9;
+        public static readonly int Headset = 10;
+        public static readonly int HwkbDploy = 11;
+        public static readonly int CameraLens = 12;
+        public static readonly int OemCustom = 13;
+        public static readonly int OemCustom2 = 14;
+        public static readonly int OemCustom3 = 15;
+        public static readonly int Count = 16;
+    }
+}
