104cc4f2eb4b6b525c681da15fd622c1a543b0a9
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2016-11-27 18:23:31 +0300

    API redesign has been finished
    - IOSCommandExecutionHelper was added
    - The new interface IPerformsTouchID was added. It is implemented by IOSDriver by default.

--- appium-dotnet-driver/Appium/Android/AndroidCommandExecutionHelper.cs
+++ appium-dotnet-driver/Appium/Android/AndroidCommandExecutionHelper.cs
@@ -12,7 +12,6 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-using Newtonsoft.Json;
 using OpenQA.Selenium.Appium.Interfaces;
 using System;
 using System.Collections.Generic;
