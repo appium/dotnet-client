a624699a81148eb6b6f5bc73e29daa7637df9858
Laolu Benson <bakinsola@gmail.com>
2018-07-21 21:04:49 +0100

    add TizenElementFactory class in order to implement missing abstract members on TizenDriver.cs (#240)

---
+++ appium-dotnet-driver/Appium/Tizen/TizenElementFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+
+namespace OpenQA.Selenium.Appium.Tizen
+{
+    public class TizenElementFactory : CachedElementFactory<TizenElement>
+    {
+        public TizenElementFactory(RemoteWebDriver parentDriver) : base(parentDriver)
+        {
+        }
+
+        protected override TizenElement CreateCachedElement(RemoteWebDriver parentDriver, string elementId)
+        {
+            return new TizenElement(parentDriver, elementId);
+        }
+    }
+}
