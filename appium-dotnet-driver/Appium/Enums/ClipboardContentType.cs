9dfd8082fadb71da6851508442bbb9caf6e8a564
Laolu Benson <bakinsola@gmail.com>
2018-10-22 03:33:50 +0100

    Add ClipboardType enum

---
+++ appium-dotnet-driver/Appium/Enums/ClipboardContentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQA.Selenium.Appium.Enums
+{
+    public enum ClipboardContentType
+    {
+        PlainText,
+        Image,
+        Url
+    }
+}
