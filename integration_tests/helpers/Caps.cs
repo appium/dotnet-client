eb3745c0ed2560b375696d1f89568d1d0c9fd55d
Laolu Benson <bakinsola@gmail.com>
2018-10-24 09:18:18 +0100

    normalize caps

--- integration_tests/helpers/Caps.cs
+++ integration_tests/helpers/Caps.cs
@@ -20,6 +20,7 @@ namespace Appium.Integration.Tests.Helpers
         public static AppiumOptions getIos112Caps(string app)
         {
             AppiumOptions capabilities = new AppiumOptions();
+            capabilities.AddAdditionalCapability(MobileCapabilityType.AutomationName, AutomationName.iOSXcuiTest);
             capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.2");
             capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "iPad Air 2");
             capabilities.AddAdditionalCapability(MobileCapabilityType.App, app);
@@ -39,9 +40,7 @@ namespace Appium.Integration.Tests.Helpers
         {
             AppiumOptions capabilities = new AppiumOptions();
             capabilities.AddAdditionalCapability(CapabilityType.BrowserName, "");
-            capabilities.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "5.0.1");
-            capabilities.AddAdditionalCapability(AndroidMobileCapabilityType.AppPackage, "io.appium.android.apis");
-            capabilities.AddAdditionalCapability(AndroidMobileCapabilityType.AppActivity, ".Apidemos");
+            capabilities.AddAdditionalCapability(MobileCapabilityType.AutomationName, AutomationName.AndroidUIAutomator2);
             capabilities.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Android Emulator");
             capabilities.AddAdditionalCapability(MobileCapabilityType.App, app);
             return capabilities;
