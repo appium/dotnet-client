4c5cf866e50ad0bd1ed28cf93db3675de344bfe2
christian.kogler <christian.kogler@anton-paar.com>
2019-09-17 14:21:36 +0200

    Added documentation to options

--- src/Appium.Net/Appium/ImageComparison/ComparisonOptions.cs
+++ src/Appium.Net/Appium/ImageComparison/ComparisonOptions.cs
@@ -4,6 +4,11 @@ namespace OpenQA.Selenium.Appium.ImageComparison
 {
     public abstract class ComparisonOptions
     {
+        /// <summary>
+        /// Makes the endpoint to return an image,
+        /// which contains the visualized result of the corresponding
+        /// picture matching operation. This option is disabled by default.
+        /// </summary>
         public bool Visualize { get; set; }
 
         public abstract Dictionary<string, object> GetParameters();
--- src/Appium.Net/Appium/ImageComparison/FeaturesMatchingOptions.cs
+++ src/Appium.Net/Appium/ImageComparison/FeaturesMatchingOptions.cs
@@ -1,11 +1,27 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
 
 namespace OpenQA.Selenium.Appium.ImageComparison
 {
     public class FeaturesMatchingOptions : ComparisonOptions
     {
+        /// <summary>
+        /// Sets the detector name for features matching algorithm.
+        /// Some of these detectors (FAST, AGAST, GFTT, FAST, SIFT and MSER) are not available
+        /// in the default OpenCV installation and have to be enabled manually before
+        /// library compilation. The default detector name is 'ORB'.
+        /// </summary>
         public string DetectorName { get; set; }
+
+        /// <summary>
+        /// The name of the matching function.
+        /// The default one is 'BruteForce'.
+        /// </summary>
         public string MatchFunc { get; set; }
+
+        /// <summary>
+        /// The maximum count of "good" matches (e. g. with minimal distances).
+        /// </summary>
         public int? GoodMatchesFactor { get; set; }
 
         public override Dictionary<string, object> GetParameters()
