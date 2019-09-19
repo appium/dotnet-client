fbaca830d462e65451cf9c4c4979e4a78965f139
koglerch13 <christiankogler@outlook.com>
2019-09-19 11:10:24 +0200

    Added Threshold property to options

--- src/Appium.Net/Appium/ImageComparison/OccurenceMatchingOptions.cs
+++ src/Appium.Net/Appium/ImageComparison/OccurenceMatchingOptions.cs
@@ -4,9 +4,20 @@ namespace OpenQA.Selenium.Appium.ImageComparison
 {
     public class OccurenceMatchingOptions : ComparisonOptions
     {
+        /// <summary>
+        /// At what normalized threshold to reject an occurrence.
+        /// Value in range 0..1. 0.5 is the default value.
+        /// </summary>
+        public double? Threshold { get; set; }
+
         public override Dictionary<string, object> GetParameters()
         {
             var parameters = new Dictionary<string, object>();
+
+            if (Threshold != null)
+            {
+                parameters.Add("threshold", Threshold);
+            }
 
             if (Visualize != null)
             {
