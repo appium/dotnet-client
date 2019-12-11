795e5f7b1908876fbcdc26fee8000111eeab599e
Laolu Benson <bakinsola@gmail.com>
2019-12-11 21:31:14 +0000

    Invoke test app to test and verify #359

--- test/integration/IOS/Device/App/ActivateAppTest.cs
+++ test/integration/IOS/Device/App/ActivateAppTest.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Appium.Net.Integration.Tests.helpers;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
 
 namespace Appium.Net.Integration.Tests.IOS.Device.App
 {
@@ -11,16 +13,20 @@ namespace Appium.Net.Integration.Tests.IOS.Device.App
     {
         private IOSDriver<IWebElement> _driver;
         private AppiumOptions _iosOptions;
-        private const string IMessageBundleId = "com.apple.MobileSMS";
-        private const string TestAppBundleId = "";// TODO find out bundleId for UICatalog
+        private const string UiCatalogAppTestAppBundleId = "com.example.apple-samplecode.UICatalog";
+        private const string IosTestAppBundleId = "io.appium.TestApp";
+        private const string _iosTestAppElement = "show alert";
+        private const string _uiCatalogTestAppElement = "Toolbars";
+
 
         [OneTimeSetUp]
         public void SetUp()
         {
             _iosOptions = Caps.GetIosCaps(Apps.Get("iosUICatalogApp"));
+            _iosOptions.AddAdditionalCapability("newCommandTimeout", 400000);
             _driver = new IOSDriver<IWebElement>(
                 Env.ServerIsLocal() ? AppiumServers.LocalServiceUri : AppiumServers.RemoteServerUri,
-                _iosOptions);
+                _iosOptions, TimeSpan.FromSeconds(120));
         }
 
         [OneTimeTearDown]
@@ -32,37 +38,38 @@ namespace Appium.Net.Integration.Tests.IOS.Device.App
         [Test]
         public void CanActivateAppTest()
         {
-            //Activate iMessage app
-            Assert.DoesNotThrow(() => _driver.ActivateApp(IMessageBundleId));
+            //Activate an app to foreground
+            Assert.DoesNotThrow(() => _driver.ActivateApp(IosTestAppBundleId));
 
-            //Verify the expected iMessage app was activated
-            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(""));
+            //Verify the expected app was activated
+            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(_iosTestAppElement));
         }
 
         [Test]
         public void CanActivateViaScriptAppTest()
         {
+            //Activate an app to foreground
             Assert.DoesNotThrow(() => _driver.ExecuteScript("mobile: activateApp",
-                new Dictionary<string, string> {{"bundleId: ", IMessageBundleId } }));
+                new Dictionary<string, string> {{"bundleId", IosTestAppBundleId}}));
 
-            //Verify the expected (iMessage) app was activated
-            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(""));
+            //Verify the expected app was activated
+            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(_iosTestAppElement));
         }
 
         [Test]
         public void CanActivateAppFromBackgroundTest()
         {
-            //Activate Messages App after test app has launched
-            _driver.ActivateApp("com.apple.MobileSMS");
-
-            //Verify the expected (iMessage) app was activated
-            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(""));
-
-            //Activates Test App to foreground from background
-            Assert.DoesNotThrow(() => _driver.ActivateApp(TestAppBundleId));
+            //Activate an app to foreground
+            _driver.ActivateApp(IosTestAppBundleId);
 
             //Verify the expected app was activated
-            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(""));
+            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(_iosTestAppElement));
+
+            //Activates Test App to foreground from background
+            Assert.DoesNotThrow(() => _driver.ActivateApp(UiCatalogAppTestAppBundleId));
+
+            //Verify the expected app was activated
+            Assert.DoesNotThrow(() => _driver.FindElementByAccessibilityId(_uiCatalogTestAppElement));
         }
     }
 }
