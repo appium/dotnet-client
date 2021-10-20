6294052fbe38e1a4f6bfdda61312326e397ed8e2
Laolu Benson <bakinsola@gmail.com>
2021-10-20 09:13:28 +0100

    chore: remove pageobjects related code and test

--- test/integration/Properties/Resources.Designer.cs
+++ test/integration/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace Appium.Net.Integration.Tests.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -16,7 +19,7 @@ namespace Appium.Net.Integration.Tests.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -63,16 +66,6 @@ namespace Appium.Net.Integration.Tests.Properties {
         public static byte[] ApiDemos_debug {
             get {
                 object obj = ResourceManager.GetObject("ApiDemos_debug", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        public static byte[] IntentExample {
-            get {
-                object obj = ResourceManager.GetObject("IntentExample", resourceCulture);
                 return ((byte[])(obj));
             }
         }
--- test/integration/helpers/Apps.cs
+++ test/integration/helpers/Apps.cs
@@ -21,7 +21,6 @@ namespace Appium.Net.Integration.Tests.helpers
                     _testApps = new Dictionary<string, string>
                     {
                         {iosTestApp, "https://github.com/appium/appium-dotnet-driver/blob/master/test/integration/apps/archives/TestApp.app.zip?raw=true"},
-                        {intentApp, "https://github.com/appium/appium-dotnet-driver/blob/master/test/integration/apps/archives/IntentExample.zip?raw=true"},
                         {iosWebviewApp, "https://github.com/appium/appium-dotnet-driver/blob/master/test/integration/apps/archives/WebViewApp.app.zip?raw=true"},
                         {iosUICatalogApp, "https://github.com/appium/appium-dotnet-driver/blob/master/test/integration/apps/archives/UICatalog.app.zip?raw=true"},
                         {androidApiDemos, "https://github.com/appium/appium-dotnet-driver/blob/master/test/integration/apps/archives/ApiDemos-debug.zip?raw=true"},
@@ -36,7 +35,6 @@ namespace Appium.Net.Integration.Tests.helpers
                     File.WriteAllBytes($"{tempFolder}/TestApp.app.zip", Resources.TestApp_app);
                     File.WriteAllBytes($"{tempFolder}/WebViewApp.app.zip", Resources.WebViewApp_app);
                     File.WriteAllBytes($"{tempFolder}/UICatalog.app.zip", Resources.UICatalog_app);
-                    File.WriteAllBytes($"{tempFolder}/IntentExample.apk", Resources.IntentExample);
                     File.WriteAllBytes($"{tempFolder}/vodqa.app.zip", Resources.vodqa);
 
 
@@ -46,7 +44,6 @@ namespace Appium.Net.Integration.Tests.helpers
                     _testApps = new Dictionary<string, string>
                     {
                         {iosTestApp, new FileInfo($"{Path.GetTempPath()}/TestApp.app.zip").FullName},
-                        {intentApp, new FileInfo($"{Path.GetTempPath()}/IntentExample.apk").FullName},
                         {iosWebviewApp, new FileInfo($"{Path.GetTempPath()}//WebViewApp.app.zip").FullName},
                         {iosUICatalogApp, new FileInfo($"{Path.GetTempPath()}/UICatalog.app.zip").FullName},
                         {androidApiDemos, new FileInfo($"{Path.GetTempPath()}/ApiDemos-debug.apk").FullName},
@@ -65,7 +62,6 @@ namespace Appium.Net.Integration.Tests.helpers
         }
 
         public static string iosTestApp = "iosTestApp";
-        public static string intentApp = "intentApp";
         public static string iosWebviewApp = "iosWebviewApp";
         public static string iosUICatalogApp = "iosUICatalogApp";
         public static string androidApiDemos = "androidApiDemos";
