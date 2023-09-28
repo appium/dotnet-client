41ecc17acc9f529347527ebfb31d9e3c494c43a0
Dor Blayzer <59066376+Dor-bl@users.noreply.github.com>
2023-09-28 10:19:03 +0300

    refactor: Improve readability for Execute in AppiumCommandExecutor.cs (#629)
    * refactor: Improve readability for Execute and add a retry logic
    
    * fix: fix broken code after rebase
    
    * feat: remove the retry mechanism, add some XML summary's

--- src/Appium.Net/Appium/Service/AppiumCommandExecutor.cs
+++ src/Appium.Net/Appium/Service/AppiumCommandExecutor.cs
@@ -12,10 +12,8 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-using Newtonsoft.Json;
 using OpenQA.Selenium.Remote;
 using System;
-using System.Collections.Generic;
 
 namespace OpenQA.Selenium.Appium.Service
 {
@@ -61,56 +59,90 @@ namespace OpenQA.Selenium.Appium.Service
 
             try
             {
-                if (commandToExecute.Name == DriverCommand.NewSession)
+                bool newSession = HandleNewSessionCommand(commandToExecute);
+                result = RealExecutor.Execute(commandToExecute);
+                if (newSession)
                 {
-                    Service?.Start();
-                    RealExecutor = ModifyNewSessionHttpRequestHeader(RealExecutor);
-
-                    result = RealExecutor.Execute(commandToExecute);
                     RealExecutor = UpdateExecutor(result, RealExecutor);
                 }
-                else
-                {
-                    result = RealExecutor.Execute(commandToExecute);
-                }
-
                 return result;
             }
             catch (Exception e)
             {
-                if ((commandToExecute.Name == DriverCommand.NewSession))
-                {
-                    Service?.Dispose();
-                }
+                HandleCommandException(commandToExecute);
 
                 throw;
             }
             finally
             {
-                if (result != null && result.Status != WebDriverResult.Success &&
-                    commandToExecute.Name == DriverCommand.NewSession)
-                {
-                    Dispose();
-                }
-
-                if (commandToExecute.Name == DriverCommand.Quit)
-                {
-                    Dispose();
-                }
+                HandleCommandCompletion(commandToExecute, result);
             }
         }
 
+        /// <summary>
+        /// Handles a new session command, starts the service, and modifies the HTTP request header if necessary.
+        /// </summary>
+        /// <param name="command">The command to be handled.</param>
+        /// <returns>True if the command is a new session command and the service was started; otherwise, false.</returns>
+        private bool HandleNewSessionCommand(Command command)
+        {
+            if (command.Name == DriverCommand.NewSession)
+            {
+                Service?.Start();
+                RealExecutor = ModifyNewSessionHttpRequestHeader(RealExecutor);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Handles the completion of a command.
+        /// If the result indicates a failure for a NewSession command, disposes the resources.
+        /// If the command is a Quit command, disposes the resources.
+        /// </summary>
+        /// <param name="command">The command that was executed.</param>
+        /// <param name="result">The result of the command execution.</param>
+        private void HandleCommandCompletion(Command command, Response result)
+        {
+            if (result != null && result.Status != WebDriverResult.Success &&
+                command.Name == DriverCommand.NewSession)
+            {
+                Dispose();
+            }
+
+            if (command.Name == DriverCommand.Quit)
+            {
+                Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Handles exceptions that occur while executing a command.
+        /// </summary>
+        /// <param name="command">The command that caused the exception.</param>
+        private void HandleCommandException(Command command)
+        {
+            if (command.Name == DriverCommand.NewSession)
+            {
+                Service?.Dispose();
+            }  
+        }
+
+        /// <summary>
+        /// Modifies the HTTP request header for a new session in the command executor.
+        /// </summary>
+        /// <param name="commandExecutor">The command executor to be modified.</param>
+        /// <returns>The modified command executor with the updated HTTP request header.</returns>
         private ICommandExecutor ModifyNewSessionHttpRequestHeader(ICommandExecutor commandExecutor)
         {
             if (commandExecutor == null) throw new ArgumentNullException(nameof(commandExecutor));
             var modifiedCommandExecutor = commandExecutor as HttpCommandExecutor;
-            
+
             modifiedCommandExecutor.SendingRemoteHttpRequest += (sender, args) =>
                     args.AddHeader(IdempotencyHeader, Guid.NewGuid().ToString());
 
             return modifiedCommandExecutor;
         }
-
 
         /// <summary>
         /// Return an instance of AppiumCommandExecutor.
@@ -121,12 +153,14 @@ namespace OpenQA.Selenium.Appium.Service
         /// <returns>A ICommandExecutor instance</returns>
         private ICommandExecutor UpdateExecutor(Response result, ICommandExecutor currentExecutor)
         {
-            if (ClientConfig.DirectConnect == false) {
+            if (ClientConfig.DirectConnect == false)
+            {
                 return currentExecutor;
             }
 
             var newExecutor = GetNewExecutorWithDirectConnect(result);
-            if (newExecutor == null) {
+            if (newExecutor == null)
+            {
                 return currentExecutor;
             }
 
@@ -136,12 +170,13 @@ namespace OpenQA.Selenium.Appium.Service
         /// <summary>
         /// Returns a new command executor if the response had directConnect.
         /// </summary>
-        /// <param name="result">The result of the command execution.</param>
+        /// <param name="response">The result of the command execution.</param>
         /// <returns>A ICommandExecutor instance or null</returns>
         private ICommandExecutor GetNewExecutorWithDirectConnect(Response response)
         {
             var newUri = new DirectConnect(response).GetUri();
-            if (newUri != null) {
+            if (newUri != null)
+            {
                 return new HttpCommandExecutor(newUri, CommandTimeout);
             }
 
