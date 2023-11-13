4eb939333ae4c6fe22872a9c42598a68c746f287
Dor-bl <dblayzer@gmail.com>
2023-11-13 23:53:41 +0200

    chore: Remove unnecessary using

--- test/integration/helpers/Npm.cs
+++ test/integration/helpers/Npm.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace Appium.Net.Integration.Tests.helpers
 {
