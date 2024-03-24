d0790e49207cd3307abbd09a120b52304a6da091
Dor Blayzer <59066376+Dor-bl@users.noreply.github.com>
2024-03-24 07:14:11 +0200

    fix: add missing XML comments for the members in the AndroidKeyMetastate class (#761)

--- src/Appium.Net/Appium/Android/Enums/AndroidKeyMetastate.cs
+++ src/Appium.Net/Appium/Android/Enums/AndroidKeyMetastate.cs
@@ -13,24 +13,94 @@
 //limitations under the License.
 namespace OpenQA.Selenium.Appium.Android.Enums
 {
+    /// <summary>
+    /// Represents the metastate for Android keys.
+    /// </summary>
     public sealed class AndroidKeyMetastate
     {
+        /// <summary>
+        /// Represents the state when the left Alt key is pressed.
+        /// </summary>
         public static readonly int Meta_Alt_LEFT_On = 16;
+
+        /// <summary>
+        /// Represents the state when the Alt key is pressed.
+        /// </summary>
         public static readonly int Meta_Alt_On = 2;
+
+        /// <summary>
+        /// Represents the state when the right Alt key is pressed.
+        /// </summary>
         public static readonly int Meta_Alt_Right_On = 32;
+
+        /// <summary>
+        /// Represents the state when the Caps Lock key is pressed.
+        /// </summary>
         public static readonly int Meta_Caps_Lock_On = 1048576;
+
+        /// <summary>
+        /// Represents the state when the left Ctrl key is pressed.
+        /// </summary>
         public static readonly int Meta_Ctrl_Left_On = 8192;
+
+        /// <summary>
+        /// Represents the state when the Ctrl key is pressed.
+        /// </summary>
         public static readonly int Meta_Ctrl_On = 4096;
+
+        /// <summary>
+        /// Represents the state when the right Ctrl key is pressed.
+        /// </summary>
         public static readonly int Meta_Ctrl_Rijht_On = 16384;
+
+        /// <summary>
+        /// Represents the state when the Function key is pressed.
+        /// </summary>
         public static readonly int Meta_Function_On = 8;
+
+        /// <summary>
+        /// Represents the state when the left Meta key is pressed.
+        /// </summary>
         public static readonly int Meta_Meta_Left_On = 131072;
+
+        /// <summary>
+        /// Represents the state when the Meta key is pressed.
+        /// </summary>
         public static readonly int Meta_Meta_On = 65536;
+
+        /// <summary>
+        /// Represents the state when the right Meta key is pressed.
+        /// </summary>
         public static readonly int Meta_Meta_Right_On = 262144;
+
+        /// <summary>
+        /// Represents the state when the Num Lock key is pressed.
+        /// </summary>
         public static readonly int Meta_Num_Lock_On = 2097152;
+
+        /// <summary>
+        /// Represents the state when the Scroll Lock key is pressed.
+        /// </summary>
         public static readonly int Meta_Scroll_Lock_On = 4194304;
+
+        /// <summary>
+        /// Represents the state when the left Shift key is pressed.
+        /// </summary>
         public static readonly int Meta_Shift_Left_On = 64;
+
+        /// <summary>
+        /// Represents the state when the Shift key is pressed.
+        /// </summary>
         public static readonly int Meta_Shift_On = 1;
+
+        /// <summary>
+        /// Represents the state when the right Shift key is pressed.
+        /// </summary>
         public static readonly int Meta_Shift_Right_On = 128;
+
+        /// <summary>
+        /// Represents the state when the Sym key is pressed.
+        /// </summary>
         public static readonly int Meta_Sym_On = 4;
     }
 }
