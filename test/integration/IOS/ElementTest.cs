9f5addd1729d1b700912c94c85ee915173951206
Dor-bl <dblayzer@gmail.com>
2024-06-21 17:59:44 +0300

    wait until elements count is 2

--- test/integration/IOS/ElementTest.cs
+++ test/integration/IOS/ElementTest.cs
@@ -1,14 +1,17 @@
-﻿using Appium.Net.Integration.Tests.helpers;
+﻿using System;
+using Appium.Net.Integration.Tests.helpers;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Support.UI;
 
 namespace Appium.Net.Integration.Tests.IOS
 {
     class ElementTests
     {
         private AppiumDriver _driver;
+        private WebDriverWait _driverWait;
 
         [OneTimeSetUp]
         public void BeforeAll()
@@ -17,6 +20,7 @@ namespace Appium.Net.Integration.Tests.IOS
             var serverUri = Env.ServerIsRemote() ? AppiumServers.RemoteServerUri : AppiumServers.LocalServiceUri;
             _driver = new IOSDriver(serverUri, capabilities, Env.InitTimeoutSec);
             _driver.Manage().Timeouts().ImplicitWait = Env.ImplicitTimeoutSec;
+            _driverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
         }
 
         [OneTimeTearDown]
@@ -44,7 +48,7 @@ namespace Appium.Net.Integration.Tests.IOS
         public void FindElementsByClassNameTest()
         {
             By byClassName = new ByClassName("XCUIElementTypeTextField");
-            Assert.That(_driver.FindElements(byClassName), Has.Count.EqualTo(2));
+            Assert.That(_driverWait.Until(_driver => _driver.FindElements(byClassName).Count == 2), Is.True);
         }
 
         [Test]
