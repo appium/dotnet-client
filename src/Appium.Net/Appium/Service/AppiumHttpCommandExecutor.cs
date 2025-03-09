44b47b70520dda0e94ff22db115c71471291d80d
Dor Blayzer <Dblayzer@gmail.com>
2025-03-09 23:50:57 +0200

    Add AppiumHttpCommandExecutor class with custom HttpClientHandler
    Introduce AppiumHttpCommandExecutor in OpenQA.Selenium.Appium.Service.
    This class inherits from HttpCommandExecutor and includes two constructors
    for initializing with a Uri and TimeSpan, with an optional AppiumClientConfig.
    Override CreateHttpClientHandler to customize HttpClientHandler with a
    callback for server certificate validation. Add ModifyHttpClientHandler
    method to return the customized handler. Include necessary using directives.

---
+++ src/Appium.Net/Appium/Service/AppiumHttpCommandExecutor.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Remote;
+using System;
+using System.Net.Http;
+
+namespace OpenQA.Selenium.Appium.Service
+{
+    public class AppiumHttpCommandExecutor : HttpCommandExecutor
+    {
+        private readonly AppiumClientConfig _clientConfig;
+
+        public AppiumHttpCommandExecutor(Uri addressOfRemoteServer, TimeSpan timeout, AppiumClientConfig clientConfig)
+          : base(addressOfRemoteServer, timeout, enableKeepAlive: true)
+        {
+            _clientConfig = clientConfig ?? throw new ArgumentNullException(nameof(clientConfig));
+        }
+
+        public AppiumHttpCommandExecutor(Uri addressOfRemoteServer, TimeSpan timeout)
+            : base(addressOfRemoteServer, timeout, enableKeepAlive: true)
+        {
+
+        }
+
+        protected override HttpClientHandler CreateHttpClientHandler()
+        {
+            var handler = base.CreateHttpClientHandler();
+
+            handler.ServerCertificateCustomValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            return handler;
+        }
+
+
+        public HttpClientHandler ModifyHttpClientHandler(AppiumClientConfig clientConfig)
+        {
+
+            return CreateHttpClientHandler();
+
+        }
+
+    }
+}
