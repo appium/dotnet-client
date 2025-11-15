18dbd450997ff20e8f1869eaa50ab87fe37c3ec1
Dor-bl <dblayzer@gmail.com>
2025-11-15 22:37:20 +0100

    chore: Remove unused using directives in IListensToLogcatMessages interface

--- src/Appium.Net/Appium/Android/Interfaces/IListensToLogcatMessages.cs
+++ src/Appium.Net/Appium/Android/Interfaces/IListensToLogcatMessages.cs
@@ -12,9 +12,7 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-using OpenQA.Selenium.Appium.WebSocket;
 using System;
-using System.Collections.Generic;
 
 namespace OpenQA.Selenium.Appium.Android.Interfaces
 {
