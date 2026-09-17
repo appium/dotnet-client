f354b4fda73823460a33ccda9382ee1a3b2ce4cc
Dor-bl <dblayzer@gmail.com>
2026-09-17 09:31:41 +0200

    test: add iOS ShakeDevice and PerformTouchID coverage
    IOSDriver.ShakeDevice() and PerformTouchID(bool) had no test coverage.
    Add DeviceInteractionTest with a Touch ID enrollment step so the
    biometric match/no-match commands can actually execute on a simulator.

---
+++ test/integration/IOS/DeviceInteractionTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Appium.Net.Integration.Tests.helpers;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Appium.iOS.Interfaces;
+
+namespace Appium.Net.Integration.Tests.IOS
+{
+    [TestFixture]
+    [Category("iOS")]
+    public class DeviceInteractionTest
+    {
+        private IOSDriver _driver;
+
+        [OneTimeSetUp]
+        public void BeforeAll()
+        {
+            var capabilities = Caps.GetIosCaps(Apps.Get("iosTestApp"));
+            var serverUri = Env.ServerIsRemote() ? AppiumServers.RemoteServerUri : AppiumServers.LocalServiceUri;
+            _driver = new IOSDriver(serverUri, capabilities, Env.InitTimeoutSec);
+            _driver.Manage().Timeouts().ImplicitWait = Env.ImplicitTimeoutSec;
+        }
+
+        [OneTimeTearDown]
+        public void AfterAll()
+        {
+            _driver?.Quit();
+            if (Env.ServerIsRemote()) return;
+            AppiumServers.StopLocalService();
+        }
+
+        [Test]
+        public void ShakeDeviceTest()
+        {
+            Assert.DoesNotThrow((Action)(() => _driver.ShakeDevice()));
+        }
+
+        [Test]
+        public void PerformTouchIdTest()
+        {
+            _driver.ExecuteScript("mobile: enrollBiometric", new Dictionary<string, object> { ["isEnabled"] = true });
+
+            var performsTouchId = (IPerformsTouchID)_driver;
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.DoesNotThrow((Action)(() => performsTouchId.PerformTouchID(true)));
+                Assert.DoesNotThrow((Action)(() => performsTouchId.PerformTouchID(false)));
+            }
+        }
+    }
+}
